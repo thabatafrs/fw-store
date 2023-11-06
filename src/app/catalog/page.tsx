7d70063ae18266136b65d3039abd40779f165a69
thabata freitas <thabatafreitas@gmail.com>
2023-11-06 13:00:37 -0300

    feat: add CatalogPage

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,26 @@
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/category-item";
+
+const CatalogPage = async () => {
+    const categories = await prismaClient.category.findMany({
+
+    })
+    return ( 
+        <div className="p-5 gap-8 flex flex-col">
+            <Badge className="w-fit gap-1 text-base border-2 uppercase border-primary px-3 py-[0.375]" variant="outline">
+                <ShapesIcon size={16}/>
+                Catálogo
+            </Badge>
+
+        <div className="grid grid-cols-2 gap-8">
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category}/>
+            ))}
+        </div>
+        </div>
+     );
+}
+ 
+export default CatalogPage;
