af19cd9070648e585124826d0ec6f205fcee6b2c
thabata freitas <thabatafreitas@gmail.com>
2023-11-06 09:54:35 -0300

    feat: add ProducItem component

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,57 +1,56 @@
 import { ProductsWithTotalPrice } from "@/helpers/products";
 import { ArrowBigDownIcon } from "lucide-react";
-import Image from "next/image"
+import Image from "next/image";
 import { Badge } from "./badge";
 
 interface ProductItemProps {
-    product: ProductsWithTotalPrice
+  product: ProductsWithTotalPrice;
 }
 
-const ProductItem = ({product}: ProductItemProps) => {
-    return ( <div className="flex flex-col gap-4 max-w-[156px]">
-        <div className="relative bg-accent rounded-lg h-[170px] w-[156px] flex items-center bg-center">
-        <Image 
-        src={product.imageUrls[0]}
-        height={0}
-        width={0}
-        sizes="100vw"
-        alt={product.name}
-        className="h-auto w-auto max-h-[70%] max-w-[80%]"
-        style={{objectFit: "contain",}}
+const ProductItem = ({ product }: ProductItemProps) => {
+  return (
+    <div className="flex max-w-[156px] flex-col gap-4">
+      <div className="relative flex h-[170px] w-[156px] items-center rounded-lg bg-accent">
+        <Image
+          src={product.imageUrls[0]}
+          height={0}
+          width={0}
+          sizes="100vw"
+          alt={product.name}
+          className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
         />
 
-        {product.discountPercentage = 0 && (
-            <Badge className="absolute top-4 left-4">
-                <ArrowBigDownIcon/> {product.discountPercentage}%
-            </Badge>
+        {product.discountPercentage > 0 && (
+          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+            <ArrowBigDownIcon size={14} /> {product.discountPercentage}%
+          </Badge>
         )}
-        </div>
+      </div>
 
-        
+      <div className="flex flex-col gap-1">
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+          {product.name}
+        </p>
 
-        <div>
-            <p className="text-ellipsis text-sm overflow-hidden whitespace-nowrap">
-                {product.name}
+        <div className="flex items-center gap-2">
+          {product.discountPercentage > 0 ? (
+            <>
+              <p className="font-semibold">
+                R$ {product.totalPrice.toFixed(2)}
+              </p>
+              <p className="text-xs line-through opacity-75">
+                R$ {Number(product.basePrice).toFixed(2)}
+              </p>
+            </>
+          ) : (
+            <p className="text-sm font-semibold">
+              R$ {product.totalPrice.toFixed(2)}
             </p>
-
-            <div className="flex items-center gap´-2">
-                {product.discountPercentage > 0 ? (
-                <>
-                <p className="font-semibold text-sm">
-                    R$ {product.totalPrice.toFixed(2)}
-                </p>
-                <p className="opacity-75 line-true text-xs">
-                    R$ {Number(product.basePrice).toFixed(2)}
-                </p>
-                </>
-                ) : (
-                    <p className="font-semibold text-sm">
-                    R$ {product.totalPrice.toFixed(2)}
-                </p>
-                )} 
-            </div>
+          )}
         </div>
-    </div> );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default ProductItem;
